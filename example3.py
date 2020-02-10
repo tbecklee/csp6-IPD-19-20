222a162d027a7515e12eb9d396ddd1e58d71ba67
Rohan Kanhai <rohan.p.kanhai@gmail.com>
2020-02-10 22:32:10 +0000

    added team name for team 10

--- example3.py
+++ example3.py
@@ -6,7 +6,7 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'E3'
+team_name = 'The Hyenas'
 strategy_name = 'Collude but retaliate'
 strategy_description = '''\
 Collude first round. Collude, except in a round after getting 
