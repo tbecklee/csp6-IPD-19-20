b568ea6e62c74f9c6d68690edffdb5104c626a87
PranavGuda <guda6922@mydusd.org>
2020-02-19 21:35:49 +0000

    We pasted our best strategy into the team 3 file

--- team3.py
+++ team3.py
@@ -10,21 +10,11 @@ team_name = 'Pranav and Natalie' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
-def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
+
+def move( my_score, their_score):
+    '''makes move based on the comparison of my score to their score
     '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
-
+    if my_score <= their_score:
+      return 'b'
+    else:
+      return 'c'
